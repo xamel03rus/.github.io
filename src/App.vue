79d3bb11b72d8d8e81cac7cd82f9a43ccce26884
xameleon03 <uurussian@mail.ru>
2020-08-22 01:17:32 +0700

    first app

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width: 100%;
+  height: 100%;
+}
+html, body {
+  background: #fef4d1;
+  height: 100%;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+}
+</style>
